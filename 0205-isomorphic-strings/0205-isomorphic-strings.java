fd8cab3a516bf75b7f3cc6eea4502bbd902fcff2
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-06-22 16:18:30 +0530

    Time: 5 ms (97.95%), Space: 43.7 MB (85.7%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,27 +1,48 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
+        
+        // Bruteforce Solution
+        // if(s.length() != t.length()){
+        //     return false;
+        // }
+        // HashMap<Character, Character> map = new HashMap<>();
+        // for(int i=0; i<s.length(); i++){
+        //     char sChar = s.charAt(i);
+        //     char tChar = t.charAt(i);
+
+        //     if(!map.containsKey(sChar)){
+        //         if(!map.containsValue(tChar)){
+        //             map.put(sChar, tChar);
+        //         }
+        //         else{
+        //             return false;
+        //         }
+        //     }
+        //     else{
+        //         char mappedCharacter = map.get(sChar);
+        //         if(mappedCharacter != tChar){
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
+
+        // Optimal Solution
+
         if(s.length() != t.length()){
             return false;
         }
-        HashMap<Character, Character> map = new HashMap<>();
+        int[] sMap = new int[256];
+        int[] tMap = new int[256];
+
         for(int i=0; i<s.length(); i++){
             char sChar = s.charAt(i);
             char tChar = t.charAt(i);
-
-            if(!map.containsKey(sChar)){
-                if(!map.containsValue(tChar)){
-                    map.put(sChar, tChar);
-                }
-                else{
-                    return false;
-                }
+            if(sMap[sChar] != tMap[tChar]){
+                return false;
             }
-            else{
-                char mappedCharacter = map.get(sChar);
-                if(mappedCharacter != tChar){
-                    return false;
-                }
-            }
+            sMap[sChar] = i+1;
+            tMap[tChar] = i+1;
         }
         return true;
     }
