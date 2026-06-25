8ecdce2cc891a29d2ceeac65a775191cabfa79aa
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-06-25 15:52:44 +0530

    Time: 4 ms (94.28%), Space: 77.2 MB (84.52%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -4,15 +4,12 @@ class Solution {
         int maxWater = Integer.MIN_VALUE;
         int i=0, j=n-1;
         while(i<j){
-            int water = (j-i) * Math.min(height[i], height[j]);
+            maxWater = Math.max(maxWater, (j-i) * Math.min(height[i], height[j]));
             if(height[i]<=height[j]){
                 i++;
             }
             else{
                 j--;
-            }
-            if(maxWater<water){
-                maxWater = water;
             }
         }
         return maxWater;
