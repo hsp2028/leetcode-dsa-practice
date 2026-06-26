6d300d562f513e2ecf5b4c256a1adaed894b7bb4
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-06-26 20:12:54 +0530

    Time: 0 ms (100%), Space: 46.4 MB (91.02%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -18,19 +18,31 @@ class Solution {
 
         // Practice Solution
 
-        int n=nums.length;
-        int k=1, i=0, j=i+1;
-        while(i<n && j<n){
-            while(j<n && nums[j] == nums[i]){
+        // int n=nums.length;
+        // int k=1, i=0, j=i+1;
+        // while(i<n && j<n){
+        //     while(j<n && nums[j] == nums[i]){
+        //         j++;
+        //     }
+        //     if(j<n){
+        //         nums[i+1] = nums[j];
+        //         i++;
+        //         j++;
+        //         k++;
+        //     }
+        // }
+        // return k;
+
+        // Optimal Solution
+
+        int n = nums.length;
+        int j=0;
+        for(int i=1; i<n; i++){
+            if(nums[j] != nums[i]){
                 j++;
-            }
-            if(j<n){
-                nums[i+1] = nums[j];
-                i++;
-                j++;
-                k++;
+                nums[j] = nums[i];
             }
         }
-        return k;
+        return j+1;
     }
 }
