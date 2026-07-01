0fb5450175941c5ec6a5b3fbf4e5985d8af81ba4
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-07-01 21:50:43 +0530

    Time: 71 ms (15.45%), Space: 46.9 MB (18.84%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    boolean isSame(int[] arr, int[] arr1){
+        for(int i=0; i<26; i++){
+            if(arr[i] != arr1[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean checkInclusion(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int[] arr = new int[26];
+        for(int i=0; i<n; i++){
+            arr[s1.charAt(i)-'a']++;
+        }
+        for(int k=0; k<m-n+1; k++){
+            int i = k+n;
+            int[] arr1 = new int[26];
+            for(int j=k; j<i; j++){
+                arr1[s2.charAt(j)-'a']++;
+            }
+            if(isSame(arr, arr1)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
