4278bfba4af52cbdfccdbd130f191b43711763d5
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-07-01 19:47:57 +0530

    Time: 0 ms (100%), Space: 44 MB (86.85%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,21 +1,42 @@
 class Solution {
     public int findPeakElement(int[] nums) {
-        int idx = -2;
-        if(nums.length == 1) return 0;
-        for(int i=0; i<nums.length; i++){
-            if(i==0){
-                if(nums[i] > nums[i+1]){
-                    return i;
-                }
-            }
-            else if(i==nums.length-1){
-                if(nums[i] > nums[i-1]){
-                    return i;
-                }
-            }
-            else if(nums[i]>nums[i+1] && nums[i] > nums[i-1]){
-                idx = i;
+        // int idx = -2;
+        // if(nums.length == 1) return 0;
+        // for(int i=0; i<nums.length; i++){
+        //     if(i==0){
+        //         if(nums[i] > nums[i+1]){
+        //             return i;
+        //         }
+        //     }
+        //     else if(i==nums.length-1){
+        //         if(nums[i] > nums[i-1]){
+        //             return i;
+        //         }
+        //     }
+        //     else if(nums[i]>nums[i+1] && nums[i] > nums[i-1]){
+        //         idx = i;
+        //         break;
+        //     }
+        // }
+        // return idx;
+
+        // Optimal Way
+        int i=1, j=nums.length-2;
+        int idx=-2;
+        if(nums.length==1) return 0;
+        if(nums[0]>nums[1]) return 0;
+        if(nums[nums.length-1]>nums[nums.length-2]) return nums.length-1;
+        while(i<=j){
+            int mid = i+(j-i)/2;
+            if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+                idx = mid;
                 break;
+            }
+            else if(nums[mid]>nums[mid-1]){
+                i = mid+1;
+            }
+            else{
+                j = mid-1;
             }
         }
         return idx;
