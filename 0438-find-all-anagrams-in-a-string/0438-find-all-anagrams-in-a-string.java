05c539a31a4df993232ba54a0546b528576bd7b0
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-07-03 22:14:25 +0530

    Time: 4 ms (99.79%), Space: 46.9 MB (59%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,34 +1,74 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
+        
+        // Bruteforce Solution -> TC = O(mn) and SC = O(1)
+        // int n = s.length();
+        // int m = p.length();
+        // List<Integer> list = new ArrayList<>();
+        // if(n<m) return list;
+        // int[] hashP = new int[26];
+        // for (int i = 0; i < m; i++) {
+        //     hashP[p.charAt(i) - 'a']++;
+        // }
+        // int i = 0;
+        // int j = 0;
+        // while (j < n) {
+        //     j = i+m;
+        //     int k = i;
+        //     int[] hashS = new int[26];
+        //     while (k < j) {
+        //         hashS[s.charAt(k)-'a']++;
+        //         k++;
+        //     }
+        //     boolean flag = true;
+        //     for(int l=0; l<26; l++){
+        //         if(hashP[l] != hashS[l]){
+        //             flag = false;
+        //             break;
+        //         }
+        //     }
+        //     if(flag){
+        //         list.add(i);
+        //     }
+        //     i++;
+        // }
+        // return list;
+
         int n = s.length();
         int m = p.length();
         List<Integer> list = new ArrayList<>();
         if(n<m) return list;
         int[] hashP = new int[26];
-        for (int i = 0; i < m; i++) {
-            hashP[p.charAt(i) - 'a']++;
+        for(int i=0; i<m; i++){
+            hashP[p.charAt(i)-'a']++;
         }
-        int i = 0;
-        int j = 0;
-        while (j < n) {
-            j = i+m;
-            int k = i;
-            int[] hashS = new int[26];
-            while (k < j) {
-                hashS[s.charAt(k)-'a']++;
-                k++;
+        int[] hashS = new int[26];
+        for(int i=0; i<m; i++){
+            hashS[s.charAt(i)-'a']++;
+        }
+        boolean flag1 = true;
+        for(int i=0; i<26; i++){
+            if(hashS[i]!=hashP[i]){
+                flag1 = false;
+                break;
             }
+        }
+        if(flag1){
+            list.add(0);
+        }
+        for(int i=m; i<n; i++){
+            hashS[s.charAt(i-m)-'a']--;
+            hashS[s.charAt(i)-'a']++;
             boolean flag = true;
-            for(int l=0; l<26; l++){
-                if(hashP[l] != hashS[l]){
+            for(int k=0; k<26; k++){
+                if(hashS[k] != hashP[k]){
                     flag = false;
                     break;
                 }
             }
             if(flag){
-                list.add(i);
+                list.add(i-m+1);
             }
-            i++;
         }
         return list;
     }
