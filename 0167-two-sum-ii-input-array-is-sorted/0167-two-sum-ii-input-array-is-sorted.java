2e56c036f75bb3cf7e3bde066e8e420667773a74
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-07-05 13:23:28 +0530

    Time: 2 ms (95.9%), Space: 48.6 MB (11.46%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -10,6 +10,28 @@ class Solution {
         // }
         // return new int[]{-1, -1};
 
+
+        // Better Solution 
+        // int n = numbers.length;
+        // for(int i=0; i<n; i++){
+        //     int l=i+1, r=n-1;
+        //     int remain = target - numbers[i];
+        //     while(l<=r){
+        //         int mid = l+(r-l)/2;
+        //         if(numbers[mid]==remain){
+        //             return new int[]{i+1, l+1};
+        //         }
+        //         else if(numbers[mid]>target){
+        //             r = mid-1;
+        //         }
+        //         else{
+        //             l = mid+1;
+        //         }
+        //     }
+        // }
+        // return new int[]{-1, -1};
+
+        // Optimal Solution
         int n = numbers.length;
         int l = 0;
         int r = n-1;
