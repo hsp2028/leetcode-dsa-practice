f4b8950ce8563167e976f279de89ab63976aadad
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-07-05 13:58:08 +0530

    Time: 4 ms (15.39%), Space: 48.5 MB (24.46%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -12,41 +12,41 @@ class Solution {
 
 
         // Better Solution 
-        // int n = numbers.length;
-        // for(int i=0; i<n; i++){
-        //     int l=i+1, r=n-1;
-        //     int remain = target - numbers[i];
-        //     while(l<=r){
-        //         int mid = l+(r-l)/2;
-        //         if(numbers[mid]==remain){
-        //             return new int[]{i+1, l+1};
-        //         }
-        //         else if(numbers[mid]>target){
-        //             r = mid-1;
-        //         }
-        //         else{
-        //             l = mid+1;
-        //         }
-        //     }
-        // }
-        // return new int[]{-1, -1};
-
-        // Optimal Solution
         int n = numbers.length;
-        int l = 0;
-        int r = n-1;
-        while(l<r){
-            int sum = numbers[l]+numbers[r];
-            if(sum == target){
-                return new int[]{l+1, r+1};
-            }
-            else if(sum<target){
-                l++;
-            }
-            else{
-                r--;
+        for(int i=0; i<n; i++){
+            int l=i+1, r=n-1;
+            int remain = target - numbers[i];
+            while(l<=r){
+                int mid = l+(r-l)/2;
+                if(numbers[mid]==remain){
+                    return new int[]{i+1, mid+1};
+                }
+                else if(numbers[mid]>remain){
+                    r = mid-1;
+                }
+                else{
+                    l = mid+1;
+                }
             }
         }
         return new int[]{-1, -1};
+
+        // Optimal Solution
+        // int n = numbers.length;
+        // int l = 0;
+        // int r = n-1;
+        // while(l<r){
+        //     int sum = numbers[l]+numbers[r];
+        //     if(sum == target){
+        //         return new int[]{l+1, r+1};
+        //     }
+        //     else if(sum<target){
+        //         l++;
+        //     }
+        //     else{
+        //         r--;
+        //     }
+        // }
+        // return new int[]{-1, -1};
     }
 }
