d1497bc702dc664ae6433c262e0c7ce3b97fe5d2
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-07-06 21:29:01 +0530

    Time: 9 ms (60.33%), Space: 47.7 MB (73.63%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -2,7 +2,7 @@ class Solution {
     public long bananaEatenAtSpeedK(int n, int[] arr){
         long count=0;
         for(int i=0; i<arr.length; i++){
-            count += (long) Math.ceil((double)arr[i]/n);
+            count += ((long)arr[i]+n-1)/n;
         }
         return count;
     }
