9b054bc441f663fc29418668d32d586d8e6abf23
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-07-06 12:25:42 +0530

    Time: 10 ms (25.94%), Space: 42 MB (51.92%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,12 +1,8 @@
 class Solution {
-    private int fibc(int n){
+    public int fib(int n) {
         if(n==0 || n==1){
             return n;
         }
-        return fibc(n-1)+fibc(n-2);
-    }
-    public int fib(int n) {
-        int result = fibc(n);
-        return result;
+        return fib(n-1)+fib(n-2);
     }
 }
