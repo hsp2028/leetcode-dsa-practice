34ebafbef58af8bc18b13cabf506302201dd3ea6
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-07-09 19:19:06 +0530

    Time: 3 ms (12.63%), Space: 43.2 MB (16.76%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.java
@@ -0,0 +1,40 @@
+class Solution {
+    private int numberOfGroups(int m, int[] nums){
+        int groupsCount=1;
+        int currentSum = 0;
+        for(int i=0; i<nums.length; i++){
+            currentSum += nums[i];
+            if(currentSum>m){
+                groupsCount++;
+                currentSum=nums[i];
+            }
+        }
+        return groupsCount;
+    }
+    public int splitArray(int[] nums, int k) {
+        int maxE = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+            if(nums[i]>maxE){
+                maxE = nums[i];
+            }
+        }
+        int low = maxE;
+        int high = sum;
+        System.out.println(low);
+        System.out.println(high);
+        int ans = 0;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(numberOfGroups(mid, nums)<=k){
+                ans = mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+}
