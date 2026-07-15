661a97525a1342c728e9bb113fb4ec8cee817895
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-07-15 22:32:57 +0530

    Time: 0 ms (100%), Space: 46.6 MB (70.24%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,17 +11,40 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        HashSet<ListNode> set = new HashSet<>();
-        ListNode temp = head;
-        while(temp != null){
-            if(temp.next == null){
-                return null;
+        
+        // Bruteforce Solution Using HashSet takes N(logN) time
+        // HashSet<ListNode> set = new HashSet<>();
+        // ListNode temp = head;
+        // while(temp != null){
+        //     if(temp.next == null){
+        //         return null;
+        //     }
+        //     if(set.contains(temp)){
+        //         return temp;
+        //     }
+        //     set.add(temp);
+        //     temp = temp.next;
+        // }
+        // return null;
+
+        // Optimal Solution Using Tortotoise and Hare
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if(fast == slow){
+                slow = head;
+
+                while(slow != fast){
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
             }
-            if(set.contains(temp)){
-                return temp;
-            }
-            set.add(temp);
-            temp = temp.next;
         }
         return null;
     }
