972af666422dc2819b0400d7dec31f2f10d2705f
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-07-15 20:09:03 +0530

    Time: 0 ms (100%), Space: 46.7 MB (52.35%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -11,17 +11,34 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashSet<ListNode> set = new HashSet<>();
-        ListNode temp = head;
-        while(temp != null){
-            if(temp.next == null){
-                return false;
-            }
-            if(set.contains(temp)){
+        
+        // Using Hashset Bruteforce Solution
+        // HashSet<ListNode> set = new HashSet<>();
+        // ListNode temp = head;
+        // while(temp != null){
+        //     if(temp.next == null){
+        //         return false;
+        //     }
+        //     if(set.contains(temp)){
+        //         return true;
+        //     }
+        //     set.add(temp);
+        //     temp = temp.next;
+        // }
+        // return false;
+
+        // Optimal Solution -> Using Tortotoise and Hare Algorithm (Floyd's Cycle)
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast){
                 return true;
             }
-            set.add(temp);
-            temp = temp.next;
         }
         return false;
     }
