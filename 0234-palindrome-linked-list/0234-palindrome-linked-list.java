adeee543529a1febc6cc06b2b8958a1a69743ad0
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-07-16 20:11:00 +0530

    Time: 3 ms (99.83%), Space: 94.1 MB (90.27%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,20 +9,53 @@
  * }
  */
 class Solution {
+    
     public boolean isPalindrome(ListNode head) {
-        Stack<Integer> stack = new Stack<>();
-        ListNode temp = head;
-        while(temp != null){
-            stack.push(temp.val);
-            temp = temp.next;
-        } 
+    // Bruteforce Solution
+    //     Stack<Integer> stack = new Stack<>();
+    //     ListNode temp = head;
+    //     while(temp != null){
+    //         stack.push(temp.val);
+    //         temp = temp.next;
+    //     } 
 
-        temp = head;
-        while(temp != null){
-            if(temp.val != stack.pop()){
+    //     temp = head;
+    //     while(temp != null){
+    //         if(temp.val != stack.pop()){
+    //             return false;
+    //         }
+    //         temp = temp.next;
+    //     }
+    //     return true;
+
+
+        // Optimal Solution
+        if(head == null){
+            return true;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+        while(fast != null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev = null;
+        while(slow != null){
+            ListNode temp = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = temp;
+        }
+        slow = prev;
+        ListNode temp = head;
+        while(slow != null && temp != null){
+            if(temp.val != slow.val){
                 return false;
             }
             temp = temp.next;
+            slow = slow.next;
         }
         return true;
     }
