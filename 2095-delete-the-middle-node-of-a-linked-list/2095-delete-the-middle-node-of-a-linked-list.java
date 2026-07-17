a58defa22dccf94dbf986445c8e5ee4ea59b4de5
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-07-17 22:37:09 +0530

    Time: 3 ms (99.96%), Space: 202.6 MB (58.26%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -12,39 +12,41 @@ class Solution {
     public ListNode deleteMiddle(ListNode head) {
         // Bruteforce Solution
 
-        ListNode temp = head;
-        int count = 0;
-        while(temp != null){
-            count++;
-            temp = temp.next;
-        }
-        if(count==1){
-            return null;
-        }
-        int idx = count/2;
-        temp = head;
-        ListNode prev = null;
-        for(int i=0; i<idx; i++){
-            prev = temp;
-            temp = temp.next;
-        }
-        prev.next = temp.next;
-        return head;
+        // ListNode temp = head;
+        // int count = 0;
+        // while(temp != null){
+        //     count++;
+        //     temp = temp.next;
+        // }
+        // if(count==1){
+        //     return null;
+        // }
+        // int idx = count/2;
+        // temp = head;
+        // ListNode prev = null;
+        // for(int i=0; i<idx; i++){
+        //     prev = temp;
+        //     temp = temp.next;
+        // }
+        // prev.next = temp.next;
+        // return head;
 
 
         // Optimal Solution
-        // ListNode fast = head;
-        // ListNode slow = head;
-        // ListNode prev = null;
-        // while(fast!=null && fast.next != null){
-        //     prev = slow;
-        //     slow = slow.next;
-        //     fast = fast.next.next;
-        // }
+        if(head.next==null) return null;
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode prev = null;
+        
+        while(fast!=null && fast.next != null){
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
         // if(prev==null){
         //     return null;
         // }
-        // prev.next = slow.next;
-        // return head;
+        prev.next = slow.next;
+        return head;
     }
 }
