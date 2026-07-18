d44e8a016295f78a7253217ee643245d130a5e5b
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-07-18 20:22:07 +0530

    Time: 1 ms (99.93%), Space: 52.7 MB (63.52%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -30,18 +30,57 @@ public class Solution {
 
         // Better Solution (Using HashSet)
 
-        HashSet<ListNode> set = new HashSet<>();
+        // HashSet<ListNode> set = new HashSet<>();
 
-        while(headA != null){
-            set.add(headA);
-            headA = headA.next;
+        // while(headA != null){
+        //     set.add(headA);
+        //     headA = headA.next;
+        // }
+
+        // while(headB != null){
+        //     if(set.contains(headB)){
+        //         return headB;
+        //     }
+        //     headB = headB.next;
+        // }
+        // return null;
+
+        // Optimal Solution
+        int l1 = 0;
+        int l2 = 0;
+        ListNode temp1 = headA;
+        ListNode temp2 = headB;
+        while(temp1 != null || temp2 != null){
+            if(temp1 != null){
+                l1++;
+                temp1 = temp1.next;
+            }
+            if(temp2 != null){
+                l2++;
+                temp2 = temp2.next;
+            }
+        }
+        int diff = l1-l2;
+
+        if(diff > 0){
+            while(diff != 0){
+                headA = headA.next;
+                diff--;
+            }
+        }
+        else{
+            while(diff != 0){
+                headB = headB.next;
+                diff++;
+            }
         }
 
-        while(headB != null){
-            if(set.contains(headB)){
+        while(headA != null){
+            if(headA == headB){
                 return headB;
             }
             headB = headB.next;
+            headA = headA.next;
         }
         return null;
     }
