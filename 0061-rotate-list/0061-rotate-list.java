ba07a3d0cd6910c4299454a2b6094acd76273a1b
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-07-21 20:25:31 +0530

    Time: 0 ms (100%), Space: 44.5 MB (26.52%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -1,63 +1,39 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
+    // Function to rotate the linked list to the right by k places
     public ListNode rotateRight(ListNode head, int k) {
-        
-        // Bruteforce Solution
-        // if(head==null || head.next==null){
-        //     return head;
-        // }
-        
-        // ListNode start = head;
-        // ListNode temp = head;
-        // int count = 0;
-        // while(temp!=null){
-        //     count++;
-        //     temp = temp.next;
-        // }
-        // k = k%count;
-        // temp = head;
-        // for(int i=0; i<k; i++){
-        //     ListNode prev = null;
-        //     while(temp.next != null){
-        //         prev = temp;
-        //         temp = temp.next;
-        //     }
-        //     temp.next = start;
-        //     start = temp;
-        //     prev.next = null;
-        // }
-        // return temp;
-
-        // Optimal Solution
-
-        if(head == null || head.next == null){
+        // If list is empty or only one node or k = 0, return head
+        if (head == null || head.next == null || k == 0)
             return head;
+
+        // Initialize length and tail
+        int length = 1;
+        ListNode tail = head;
+
+        // Traverse to find tail and calculate length
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
         }
 
-        ListNode temp = head;
-        int count = 1;
-        while(temp.next != null){
-            count++;
-            temp = temp.next;
-        }
-        temp.next = head;
-        k = k%count;
-        int n = count-k;
+        // Make the list circular
+        tail.next = head;
 
-        for(int i=0; i<n; i++){
-            temp = temp.next;
+        // Calculate effective rotation
+        k = k % length;
+
+        // Traverse to new tail position
+        int stepsToNewTail = length - k;
+        ListNode newTail = head;
+        for (int i = 1; i < stepsToNewTail; i++) {
+            newTail = newTail.next;
         }
-        head = temp.next;
-        temp.next = null;
-        return head;
+
+        // New head is next of newTail
+        ListNode newHead = newTail.next;
+
+        // Break the circle
+        newTail.next = null;
+
+        return newHead;
     }
 }
