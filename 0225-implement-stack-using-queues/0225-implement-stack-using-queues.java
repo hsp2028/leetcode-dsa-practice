6af6d46969b1f9c648481541269e7fb79951d047
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-07-25 16:02:24 +0530

    Time: 0 ms (100%), Space: 42.9 MB (48.31%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,28 +1,67 @@
 class MyStack {
-    Queue<Integer> q;
+    
+    // Bruteforce Solution
+    // Queue<Integer> q;
 
-    public MyStack() {
-        q = new LinkedList();
-    }
+    // public MyStack() {
+    //     q = new LinkedList();
+    // }
     
-    public void push(int x) {
-        q.add(x);
-        for(int i=0; i<q.size()-1; i++){
-            q.add(q.remove());
+    // public void push(int x) {
+    //     q.add(x);
+    //     for(int i=0; i<q.size()-1; i++){
+    //         q.add(q.remove());
+    //     }
+    // }
+    
+    // public int pop() {
+    //     return q.remove();
+    // }
+    
+    // public int top() {
+    //     return q.peek();
+    // }
+    
+    // public boolean empty() {
+    //     return q.isEmpty();
+    // }
+
+
+    // Optimal Solution
+
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+    int topElement;
+
+    public MyStack(){
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+    public void push(int x){
+        q1.add(x);
+        topElement = x;
+    }
+
+    public int pop(){
+        while(q1.size()>1){
+            topElement = q1.remove();
+            q2.add(topElement);
         }
+        int removed = q1.remove();
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+        return removed;
     }
-    
-    public int pop() {
-        return q.remove();
+
+    public int top(){
+        return topElement;
     }
-    
-    public int top() {
-        return q.peek();
+
+    public boolean empty(){
+        return q1.isEmpty();
     }
-    
-    public boolean empty() {
-        return q.isEmpty();
-    }
+
 }
 
 /**
