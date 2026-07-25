00933e66125eee6a2faed61c26ac41c9e93365b4
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-07-25 15:47:41 +0530

    Time: 1 ms (67.95%), Space: 42.9 MB (30.29%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,35 +1,41 @@
-class MyStack {
-    Queue<Integer> q;
+import java.util.LinkedList;
+import java.util.Queue;
 
-    public MyStack() {
-        q = new LinkedList();
-    }
-    
+class MyStack {
+    private Queue<Integer> q1 = new LinkedList<>();
+    private Queue<Integer> q2 = new LinkedList<>();
+    private int topElement;
+
+    // O(1) Time
     public void push(int x) {
-        q.add(x);
-        for(int i=0; i<q.size()-1; i++){
-            q.add(q.remove());
-        }
+        q1.add(x);
+        topElement = x; // Track top element for O(1) top() reads
     }
     
+    // O(N) Time
     public int pop() {
-        return q.remove();
+        // Move all elements except the last one to q2
+        while (q1.size() > 1) {
+            topElement = q1.remove();
+            q2.add(topElement);
+        }
+        int removed = q1.remove(); // The last element is the LIFO target
+        
+        // Swap the queues
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+        
+        return removed;
     }
     
+    // O(1) Time (due to tracking variable)
     public int top() {
-        return q.peek();
+        return topElement;
     }
     
+    // O(1) Time
     public boolean empty() {
-        return q.isEmpty();
+        return q1.isEmpty();
     }
 }
-
-/**
- * Your MyStack object will be instantiated and called as such:
- * MyStack obj = new MyStack();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.top();
- * boolean param_4 = obj.empty();
- */
