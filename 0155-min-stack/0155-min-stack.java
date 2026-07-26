9fbf8f64c53155bd24a11c6ab598fb0508b2c98e
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-07-26 19:59:26 +0530

    Time: 29 ms (84.33%), Space: 102.3 MB (21.18%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,28 +1,76 @@
 class MinStack {
-    Stack<int[]> stack;
-    public MinStack() {
+    // Stack<int[]> stack;
+    // public MinStack() {
+    //     stack = new Stack<>();
+    // }
+    
+    // public void push(int value) {
+    //     if(stack.isEmpty()){
+    //         stack.push(new int[] {value, value});
+    //         return;
+    //     }
+    //     int mini = Math.min(getMin(), value);
+    //     stack.push(new int[] {value, mini});
+    // }
+    
+    // public void pop() {
+    //     stack.pop();
+    // }
+    
+    // public int top() {
+    //     return stack.peek()[0];
+    // }
+    
+    // public int getMin() {
+    //     return stack.peek()[1];
+    // }
+
+    // Optimal solution using (2*val-prevVal)
+
+    Stack<Long> stack;
+    long min;
+
+    public MinStack(){
         stack = new Stack<>();
     }
-    
-    public void push(int value) {
+
+    public void push(int val){
+        long x = val;
         if(stack.isEmpty()){
-            stack.push(new int[] {value, value});
+            min = x;
+            stack.push(x);
             return;
         }
-        int mini = Math.min(getMin(), value);
-        stack.push(new int[] {value, mini});
+
+        if(x>=min){
+            stack.push(x);
+        }
+        else{
+            stack.push(2*x-min);
+            min = x;
+        }
     }
-    
-    public void pop() {
-        stack.pop();
+    public void pop(){
+        if(stack.isEmpty()){
+            return;
+        }
+        long popedVal = stack.pop();
+        if(popedVal<min){
+            min = 2*min-popedVal;
+        }
     }
-    
-    public int top() {
-        return stack.peek()[0];
+
+    public int top(){
+        if(stack.isEmpty()){
+            return -1;
+        }
+        long x = stack.peek();
+        if(min<x) return (int) x;
+        return (int) min;
     }
-    
-    public int getMin() {
-        return stack.peek()[1];
+
+    public int getMin(){
+        return (int) min;
     }
 }
 
