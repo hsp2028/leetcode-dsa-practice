6680280c6da1908bd321a0846d3733ec930d2469
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-07-27 20:13:58 +0530

    Time: 13 ms (74.06%), Space: 48.5 MB (60.28%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -1,18 +1,45 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
-        int n = nums.length;
-        int[] res = new int[n];
-        Arrays.fill(res, -1);
+        // Bruteforce Solution
+        // int n = nums.length;
+        // int[] res = new int[n];
+        // Arrays.fill(res, -1);
 
-        for(int i=0; i<n; i++){
-            int crrE = nums[i];
-            for(int j=1; j<n; j++){
-                int idx = (i+j)%n;
-                if(nums[idx]>nums[i]){
-                    res[i] = nums[idx];
-                    break;
+        // for(int i=0; i<n; i++){
+        //     int crrE = nums[i];
+        //     for(int j=1; j<n; j++){
+        //         int idx = (i+j)%n;
+        //         if(nums[idx]>nums[i]){
+        //             res[i] = nums[idx];
+        //             break;
+        //         }
+        //     }
+        // }
+        // return res;
+
+        // Optimal Solution 
+
+        int n = nums.length;
+        Stack<Integer> stack = new Stack<>();
+
+        int[] res = new int[n];
+        for(int i=2*n-1; i>=0; i--){
+            int idx = i%n;
+            int currE = nums[idx];
+
+            while(!stack.isEmpty() && stack.peek()<=currE){
+                stack.pop();
+            }
+
+            if(i<n){
+                if(stack.isEmpty()){
+                    res[idx] = -1;
+                }
+                else{
+                    res[idx] = stack.peek();
                 }
             }
+            stack.push(currE);
         }
         return res;
     }
