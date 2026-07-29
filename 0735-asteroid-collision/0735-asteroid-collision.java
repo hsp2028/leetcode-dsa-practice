6faee2acf4677d1986a217bb027d79e6ad170971
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-07-29 20:34:41 +0530

    Time: 12 ms (5.14%), Space: 47.8 MB (5.23%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -8,19 +8,12 @@ class Solution {
                 stack.push(asteroids[i]);
             }
             else{
-                // if(!stack.isEmpty() && stack.peek()==Math.abs(asteroids[i])){
-                //     stack.pop();
-                //     continue;
-                // }
                 boolean flag = true;
-                int count = 0;
                 while(!stack.isEmpty() && stack.peek()<=Math.abs(asteroids[i])){
                     if(stack.peek()==Math.abs(asteroids[i])){
                         stack.pop();
                         System.out.println(stack);
                         flag = false;
-                        count++;
-                        System.out.println(count);
                         break;
                     }
                     stack.pop();
@@ -30,11 +23,9 @@ class Solution {
                 }
             }
         }
-        System.out.println(stack);
         for(int num : stack){
             list.add(num);
         }
-        // System.out.println(list);
         int[] res = list.stream().mapToInt(Integer::intValue).toArray();
         return res;
     }
