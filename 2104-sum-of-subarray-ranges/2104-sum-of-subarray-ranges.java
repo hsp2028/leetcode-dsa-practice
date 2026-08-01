ed47e3545abffbc4a9f6b78680300dc652df8766
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-08-01 09:32:03 +0530

    Time: 44 ms (16.58%), Space: 46.5 MB (51.27%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,16 +1,93 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
+        
+        // Bruteforce Solution
+        // int n = nums.length;
+        // long rangeSum = 0;
+        // for(int i=0; i<n; i++){
+        //     int max = Integer.MIN_VALUE;
+        //     int min = Integer.MAX_VALUE;
+        //     for(int j=i; j<n; j++){
+        //         max = Math.max(max, nums[j]);
+        //         min = Math.min(min, nums[j]);
+        //         rangeSum += (long)(max-min);
+        //     }
+        // }
+        // return rangeSum;
+
+        // Optimal Solution
         int n = nums.length;
-        long rangeSum = 0;
+        int[] nse = findNSE(nums);
+        int[] psee = findPSEE(nums);
+        int[] nge = findNGE(nums);
+        int[] pgee = findPGEE(nums);
+        long minSum = 0;
+        long maxSum = 0;
         for(int i=0; i<n; i++){
-            int max = Integer.MIN_VALUE;
-            int min = Integer.MAX_VALUE;
-            for(int j=i; j<n; j++){
-                max = Math.max(max, nums[j]);
-                min = Math.min(min, nums[j]);
-                rangeSum += (long)(max-min);
-            }
+            int left = i-psee[i];
+            int right = nse[i]-i;
+
+            long freq = left*right*1L;
+            minSum += freq*nums[i];
         }
-        return rangeSum;
+        for(int i=0; i<n; i++){
+            int left = i-pgee[i];
+            int right = nge[i]-i;
+            long freq = left*right*1L;
+            maxSum += freq*nums[i];
+        }
+        return (maxSum-minSum);
+    }
+    private int[] findNSE(int[] nums){
+        int n = nums.length;
+        Stack<Integer> stack = new Stack<>();
+        int[] res = new int[n];
+        for(int i=n-1; i>=0; i--){
+            while(!stack.isEmpty() && nums[i]<=nums[stack.peek()]){
+                stack.pop();
+            }
+            res[i] = !stack.isEmpty()?stack.peek():n;
+            stack.push(i);
+        }
+        return res;
+    }
+    private int[] findPSEE(int[] nums){
+        int n = nums.length;
+        Stack<Integer> stack = new Stack<>();
+        int[] res = new int[n];
+        for(int i=0; i<n; i++){
+            while(!stack.isEmpty() && nums[i]<nums[stack.peek()]){
+                stack.pop();
+            }
+            res[i] = !stack.isEmpty()?stack.peek():-1;
+            stack.push(i);
+        }
+        return res;
+    }
+    private int[] findNGE(int[] nums){
+        int n = nums.length;
+        Stack<Integer> stack = new Stack<>();
+        int[] res = new int[n];
+        for(int i=n-1; i>=0; i--){
+            while(!stack.isEmpty() && nums[i]>=nums[stack.peek()]){
+                stack.pop();
+            }
+            res[i] = !stack.isEmpty()?stack.peek():n;
+            stack.push(i);
+        }
+        return res;
+    }
+    private int[] findPGEE(int[] nums){
+        int n = nums.length;
+        Stack<Integer> stack = new Stack<>();
+        int[] res = new int[n];
+        for(int i=0; i<n; i++){
+            while(!stack.isEmpty() && nums[stack.peek()]<nums[i]){
+                stack.pop();
+            }
+            res[i] = !stack.isEmpty()?stack.peek():-1;
+            stack.push(i);
+        }
+        return res;
     }
 }
