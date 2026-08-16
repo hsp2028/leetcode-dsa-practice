7c6f5834979d24856dd0a433ef9db45963460c5a
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-08-16 23:54:02 +0530

    Time: 2 ms (91.98%), Space: 47.7 MB (76.65%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -26,26 +26,37 @@ class Solution {
         // for(int i=count; i<n; i++){
         //     nums[i] = 0;
         // }
-
+        int n = nums.length;
+        int[] a = new int[n];
+        int j = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i]!=0){
+                a[j] = nums[i]; 
+                j++;
+            }
+        }
+        for(int i=0; i<n; i++){
+            nums[i] = a[i];
+        }
         // Optimal Solution
 
-        int n = nums.length;
-        int j = -1;
-        for(int i=0; i<n; i++){
-            if(nums[i]==0){
-                j = i;
-                break;
-            }
-        }
-        if(j!=-1){
-            for(int i=j+1; i<n; i++){
-                if(nums[i]!=0){
-                    int temp = nums[i];
-                    nums[i] = nums[j];
-                    nums[j] = temp;
-                    j++;
-                }
-            }
-        }
+        // int n = nums.length;
+        // int j = -1;
+        // for(int i=0; i<n; i++){
+        //     if(nums[i]==0){
+        //         j = i;
+        //         break;
+        //     }
+        // }
+        // if(j!=-1){
+        //     for(int i=j+1; i<n; i++){
+        //         if(nums[i]!=0){
+        //             int temp = nums[i];
+        //             nums[i] = nums[j];
+        //             nums[j] = temp;
+        //             j++;
+        //         }
+        //     }
+        // }
     }
 }
