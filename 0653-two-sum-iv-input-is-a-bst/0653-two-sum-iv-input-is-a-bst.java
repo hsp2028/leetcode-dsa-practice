7a97dfa695cd3261f088874f266922c11fd8ffd6
Harshit Singh Parmar <harshitsinghp8@gmail.com>
2026-09-15 22:13:25 +0530

    Time: 2 ms (98.09%), Space: 47.2 MB (54.36%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -14,22 +14,47 @@
  * }
  */
 class Solution {
+    public void inorder(TreeNode root, List<Integer> list){
+        if(root == null){
+            return;
+        }
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
     public boolean findTarget(TreeNode root, int k) {
-        HashSet<Integer> set = new HashSet<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            TreeNode node = q.poll();
-            int remain = k-node.val;
-            if(set.contains(remain)){
+        // HashSet<Integer> set = new HashSet<>();
+        // Queue<TreeNode> q = new LinkedList<>();
+        // q.add(root);
+        // while(!q.isEmpty()){
+        //     TreeNode node = q.poll();
+        //     int remain = k-node.val;
+        //     if(set.contains(remain)){
+        //         return true;
+        //     }
+        //     set.add(node.val);
+        //     if(node.left!=null){
+        //         q.add(node.left);
+        //     }
+        //     if(node.right!=null){
+        //         q.add(node.right);
+        //     }
+        // }
+        // return false;
+
+        // Better Approach
+        List<Integer> list = new ArrayList<>();
+        inorder(root, list);
+        int i=0, j=list.size()-1;
+        while(i<j){
+            if(list.get(i)+list.get(j)<k){
+                i++;
+            }
+            else if(list.get(i)+list.get(j)>k){
+                j--;
+            }
+            else{
                 return true;
-            }
-            set.add(node.val);
-            if(node.left!=null){
-                q.add(node.left);
-            }
-            if(node.right!=null){
-                q.add(node.right);
             }
         }
         return false;
